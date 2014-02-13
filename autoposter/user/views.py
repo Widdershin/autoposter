c1b570f3102f3b5e454256fd21dcbca1b464878f
Nick Johnstone <ncwjohnstone@gmail.com>
2014-02-13 23:59:44 +1300

    Added users/add_post view and template

--- autoposter/user/views.py
+++ autoposter/user/views.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask.ext.login import login_required, current_user
-from autoposter.user.models import User
+from autoposter.user.models import Post
+from .forms import NewPostForm
 
 blueprint = Blueprint("user", __name__, url_prefix='/users',
-                        static_folder="../static")
+                      static_folder="../static")
 
 
 @blueprint.route("/posts")
 @login_required
 def posts():
     return render_template("users/posts.html", posts=current_user.posts)
+
+
+@blueprint.route("/posts/add", methods=('GET', 'POST'))
+@login_required
+def add_post():
+    form = NewPostForm()
+
+    if form.validate_on_submit():
+        new_post = Post(**{k: v for k, v in form.data.iteritems() if v})
+
+        new_post.save()
+
+        current_user.posts.append(new_post)
+        current_user.save()
+
+        return redirect(url_for('user.posts'))
+
+    return render_template("users/newpost.html", form=form)
