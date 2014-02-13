b0e3b287e8f0dcc97882690d27b1c0d6e22e38ad
Nick Johnstone <ncwjohnstone@gmail.com>
2014-02-13 23:59:27 +1300

    Added NewPostForm model form for Post creation

--- autoposter/user/forms.py
+++ autoposter/user/forms.py
@@ -1,19 +1,23 @@
 from flask_wtf import Form
 from wtforms import TextField, PasswordField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
-from wtforms.ext.appengine.db import model_form
+from wtforms.ext.sqlalchemy.orm import model_form
+from .models import User, Post
+from autoposter.database import db
 
-from .models import User, Post, DaysOfWeek
 
 class RegisterForm(Form):
     username = TextField('Username',
-                    validators=[DataRequired(), Length(min=3, max=25)])
-    email = TextField('Email',
-                    validators=[DataRequired(), Email(), Length(min=6, max=40)])
-    password = PasswordField('Password',
-                                validators=[DataRequired(), Length(min=6, max=40)])
-    confirm = PasswordField('Verify password',
-                [DataRequired(), EqualTo('password', message='Passwords must match')])
+                         validators=[DataRequired(), Length(min=3, max=25)])
+
+    email = TextField('Email', validators=[
+        DataRequired(), Email(), Length(min=6, max=40)])
+
+    password = PasswordField('Password', validators=[
+        DataRequired(), Length(min=6, max=40)])
+
+    confirm = PasswordField('Verify password', [
+        DataRequired(), EqualTo('password', message='Passwords must match')])
 
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
@@ -34,3 +38,5 @@ class RegisterForm(Form):
 
         self.user = user
         return True
+
+NewPostForm = model_form(Post, db_session=db.session, base_class=Form)
