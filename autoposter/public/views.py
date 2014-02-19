2199741d0af73c43d7366bf8d239dd4956d86cb7
Nick Johnstone <ncwjohnstone@gmail.com>
2014-02-19 22:47:58 +1300

    pep8 changes in public/views

--- autoposter/public/views.py
+++ autoposter/public/views.py
@@ -13,6 +13,7 @@ from autoposter.database import db
 
 blueprint = Blueprint('public', __name__, static_folder="../static")
 
+
 @login_manager.user_loader
 def load_user(id):
     return User.get_by_id(int(id))
@@ -32,6 +33,7 @@ def home():
             flash_errors(form)
     return render_template("public/home.html", form=form)
 
+
 @blueprint.route('/logout/')
 @login_required
 def logout():
@@ -39,19 +41,21 @@ def logout():
     flash('You are logged out.', 'info')
     return redirect(url_for('public.home'))
 
+
 @blueprint.route("/register/", methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form, csrf_enabled=False)
     if form.validate_on_submit():
         new_user = User.create(username=form.username.data,
-                        email=form.email.data,
-                        password=form.password.data,
-                        active=True)
+                               email=form.email.data,
+                               password=form.password.data,
+                               active=True)
         flash("Thank you for registering. You can now log in.", 'success')
         return redirect(url_for('public.home'))
     else:
         flash_errors(form)
     return render_template('public/register.html', form=form)
+
 
 @blueprint.route("/about/")
 def about():
